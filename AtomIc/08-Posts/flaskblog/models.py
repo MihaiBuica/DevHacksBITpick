e55e5b0af40c55f909c80aa20a39f692ead8e09c
Mihai Draghici <mihai.draghici98@gmail.com>
2019-11-23 20:44:40 +0200

    afisare topicuri in lista principala. afisare mai multe detalii. optiune payment.

--- AtomIc/08-Posts/flaskblog/models.py
+++ AtomIc/08-Posts/flaskblog/models.py
@@ -25,10 +25,11 @@ class User(db.Model, UserMixin):
 
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(100), nullable=False)
+	sum = db.Column(db.Integer, nullable=False)
 	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-	content = db.Column(db.Text, nullable=False)
+	interest = db.Column(db.Integer, nullable=False)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-	date = db.Column(db.Text)
+	payDate = db.Column(db.Date, nullable=False)
+	description = db.Column(db.Text)
 	def __repr__(self):
 		return f"Post('{self.title}', '{self.date_posted}')"
